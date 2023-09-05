1c819849a8384eef4a7726d650095437a3163841
rintosaputro <saputrorinto@gmail.com>
2023-09-05 13:15:37 +0700

    fix: link header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ButtonLink from "./ButtonLink";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="p-5 bg-[#4FBDBA] flex justify-center gap-4">
-      <ButtonLink href={"/"} className="p-3 bg-white rounded-lg">
+      <Link href={"/"} className="p-3 bg-white rounded-lg">
         Home
-      </ButtonLink>
+      </Link>
       <ButtonLink href="/users" className="p-3 bg-white rounded-lg">
         Users
       </ButtonLink>
